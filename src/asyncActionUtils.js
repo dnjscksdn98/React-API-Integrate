9017ce2fab55860df4788c92b6ac4022878f6a14
Alex <dnjscksdn98@gmail.com>
2020-01-23 16:47:38 +0900

    Refactor user context

---
+++ src/asyncActionUtils.js
@@ -0,0 +1,82 @@
+export default function createAsyncDispatcher(type, promiseFn) {
+  const SUCCESS = `${type}_SUCCESS`;
+  const ERROR = `${type}_ERROR`;
+
+  async function actionHandler(dispatch, ...rest) {
+    dispatch({ type });
+    try {
+      const data = await promiseFn(...rest);
+      dispatch({
+        type: SUCCESS,
+        data
+      });
+    } catch (e) {
+      dispatch({
+        type: ERROR,
+        error: e
+      });
+    }
+  }
+
+  return actionHandler;
+}
+
+// initial state
+export const initialAsyncState = {
+  loading: false,
+  data: null,
+  error: null
+};
+
+// action object
+const loadingState = {
+  loading: true,
+  data: null,
+  error: null
+};
+
+const success = data => ({
+  loading: false,
+  data,
+  error: null
+});
+
+const error = e => ({
+  loading: false,
+  data: null,
+  error: e
+});
+
+// type: action type
+// key: users, user
+export function createAsyncHandler(type, key) {
+  const SUCCESS = `${type}_SUCCESS`;
+  const ERROR = `${type}_ERROR`;
+
+  function handler(state, action) {
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: loadingState
+        };
+
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: success(action.data)
+        };
+
+      case ERROR:
+        return {
+          ...state,
+          [key]: error(action.error)
+        };
+
+      default:
+        return state;
+    }
+  }
+
+  return handler;
+}
